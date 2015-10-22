4f06e9faebc5241225ccde1821ab014cc4e63516
halismai@cs.cmu.edu <halismai@cs.cmu.edu>
2015-10-22 14:55:14 -0400

    ios things to delete

---
+++ ios/BitPlanes.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <Accelerate/Accelerate.h>
+
+#include "bitplanes/core/bitplanes_tracker_pyramid.h"
+
+@interface BitPlanes : NSObject
+
+@end
+
---
+++ ios/BitPlanes.mm
@@ -0,0 +1,5 @@
+#import "BitPlanes.h"
+
+@implementation BitPlanes
+
+@end
